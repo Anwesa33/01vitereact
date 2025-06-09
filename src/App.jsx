b8b5e2cf68e7253381bc2cfd48979571015453be
Anwesa Ray <anwesaray26@gmail.com>
2025-06-10 01:40:52 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { useState } from 'react';
 
 function App() {
+
+  let [counter, setCounter] = useState(5);
   
+  // let counter = 5;
+
+  // const addValue = () => {
+  //   if (counter < 20){setCounter(counter + 1)}
+  //   else {
+  //     alert("Counter cannot be greater than 20");
+  //   }
+    
+  // }
+
+  // const removeValue = () => {
+  //   if (counter > 0){setCounter(counter - 1)}
+  //   else {
+  //     alert("Counter cannot be lesser than 0");
+  //   }
+  // }
 
   return (
     <>
-      <h1>Chai aur React with vite | Anwesa Ray</h1>
+      <h1>Chai aur React with vite</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button disabled={counter >= 20} onClick={() => counter < 20 ? setCounter(counter + 1) : null}>➕</button> <br />
+      <button disabled={counter <= 0} onClick={() => counter > 0 ? setCounter(counter - 1) : null}>➖</button>
+
+      <p>Footer: {counter}</p>
     </>
   )
 }
